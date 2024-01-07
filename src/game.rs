e600c2b1d3312ce163adb8e03b1df6948bd481af
itsmepravin <singh.pravin19@yahoo.com>
2024-01-07 11:51:17 +0545

    game window creation

---
+++ src/game.rs
@@ -0,0 +1,66 @@
+use piston_window::types::Color;
+use piston_window::*;
+
+use rand::{thread_rng, Rng};
+
+use crate::draw::{draw_block, draw_rect};
+use crate::snake::{Direction, Snake};
+
+const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
+const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
+const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
+
+pub struct Game {
+    snake: Snake,
+
+    food_exists: bool,
+    food_x: i32,
+    food_y: i32,
+
+    width: i32,
+    height: i32,
+
+    game_over: bool,
+    waiting_time: f64,
+}
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Game {
+        Game {
+            snake: Snake::new(2, 2),
+
+            food_exists: true,
+            food_x: 6,
+            food_y: 4,
+
+            width,
+            height,
+
+            game_over: false,
+            waiting_time: 0.0,
+        }
+    }
+
+    pub fn key_pressed(&mut self, key: Key) {
+        if self.game_over {
+            return;
+        }
+
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Left => Some(Direction::Left),
+            Key::Right => Some(Direction::Right),
+            _ => None,
+        };
+
+        if dir.unwrap() == self.snake.head_direction().opposite() {
+            return;
+        }
+
+        // self.update_snake(dir);
+    }
+}
